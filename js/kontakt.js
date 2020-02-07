89b5d135ffd99c6a7a6e450feb07026c015b6df6
Ellinor <57523353+EllinorBrandin@users.noreply.github.com>
2020-02-07 10:42:33 +0100

    fixar med html etc

--- js/kontakt.js
+++
@@ -1,9 +0,0 @@
-const nav = document.querySelector('.navLinks')
-const burger = document.querySelector('.burger')
-
-function toggleMeny() {
-  nav.classList.toggle('navActive')
- burger.classList.toggle('toggle')
-
-}
-burger.addEventListener('click', toggleMeny)
